ba63ddd1ed7ef2e23d77b91efb610ff7bd86bd7e
Lindsay Johnston <lindsaykjohnston@outlook.com>
2021-02-01 13:50:05 -0600

    Styled navlinks

--- src/App.js
+++ src/App.js
@@ -13,20 +13,42 @@ class App extends Component {
             <h1 >VANNA OH!</h1>
 
             <nav >
-              <NavLink to="/" exact activeClassName={styles.Selected} className={styles.NavLink}>HOME</NavLink>
-              <NavLink to="/bio" exact activeClassName={styles.Selected} className={styles.NavLink}>BIO</NavLink>
-              <NavLink to="/music" exact activeClassName={styles.Selected} className={styles.NavLink}>MUSIC</NavLink>
+              <NavLink 
+                to="/" 
+                exact 
+                activeClassName={styles.Selected} 
+                className={styles.NavLink}>HOME</NavLink>
+              <NavLink 
+                to="/bio" 
+                exact 
+                activeClassName={styles.Selected} 
+                className={styles.NavLink}>BIO</NavLink>
+              <NavLink 
+                to="/music" 
+                exact 
+                activeClassName={styles.Selected} 
+                className={styles.NavLink}>MUSIC</NavLink>
             </nav>
           </header>
           <div className={styles.PageArea}>
-            <Route path="/bio" exact render={()=>(<h1>BIO</h1>)}></Route>
-            <Route path="/music" exact render={()=>(
+            <Route 
+              path="/bio" 
+              exact 
+              render={()=>(<div>BIO</div>)}></Route>
+
+            <Route 
+              path="/music" 
+              exact 
+              render={()=>(
               <div>MUSIC</div>
             )}></Route>
-            <Route path="/" exact render={(props)=>(
+
+            <Route 
+              path="/" 
+              exact 
+              render={(props)=>(
               <div>HOME STUFF</div>
             )}></Route>
-
           </div>
         </div>
 
